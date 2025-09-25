5cc522445aed19ff20b7a5d4343964b32c4867fb
seokjinjuseyyo <seokjinjuseyyo1@gmail.com>
2025-09-25 13:03:42 +0700

    second commit

---
+++ A1-2025/pertemuan-2/pertemuan2.py
@@ -0,0 +1,54 @@
+# # nilai = 70 
+
+# status = lulus if nilai 
+
+# # if nilai > 75: 
+# #     print ('Nilai A')
+# # elif nilai > 65:
+# #     print ('nilai b')
+# # else:
+# #     print ('nilai c')
+
+
+# # suhu = 35
+# # if suhu > 35:
+#     print ('panas')
+# if suhu > 30:
+#     print ('sauna')
+# elif suhu > 20:
+#     print ('normal')
+# else:
+#     print ('dingin')
+
+
+
+# umur = int(input("Masukkan umur Anda: "))
+# if umur < 13:
+# kategori = "Anak-anak"
+# elif umur < 18:
+# kategori = "Remaja"
+# elif umur < 60:
+# kategori = "Dewasa"
+# else:
+# kategori = "Lansia"
+# print("Umur:", umur, "Kategori:", kategori)
+
+# tinggi = float(input('masukan tinggi badan anda\t:'))
+# if tinggi >= 145: 
+#     print ('boleh naik')
+# else:
+#     tinggi < 145:
+#     print ('tidak boleh naik')
+# bisakah = 
+
+
+harga = float(input('masukan total harga\t:'))
+if total > 100.000: 
+    print ('diskon 20%')
+elif:
+    total > 50.000:
+    print ('diskon 10%')
+else:
+    print ('harga normal')
+jadi = 'diskon 20%' if total > 100.000  total > 50.000 'diskon 10%' else 'harga normal'
+print (jadi)
